a26c99a637ccccb5be6cf50f023406545f2e5d22
bopan <bopan@isco.com>
2018-08-22 18:26:30 -0700

    initial check in for all stuff

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=li-hire-assistant-Web-Deploy.pubxml -p:Password=ij7SkGtyYJeKflhG4dFzAQ2hkvHGg9EXiauz3t71sbdHi8uAR9kfpeFSA9Rb
+
